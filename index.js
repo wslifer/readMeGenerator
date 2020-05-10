0c37b47e20c54f9ce644e1362c975230186e13ce
Wesley Slifer <wesleyslifer@ymail.com>
2020-05-10 17:16:55 -0500

    added dotenv and installed all dependencies

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const questions = [
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-];
+const questions = [];
 
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
-function init() {
-
-}
+function init() {}
 
 init();
