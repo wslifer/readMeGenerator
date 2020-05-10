ce2b729894187c5d48bfb590001c8da44736d961
Wesley Slifer <wesleyslifer@ymail.com>
2020-05-10 17:50:32 -0500

    attempted to add prompts for users

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 const fs = require("fs");
-const axios = require("axios");
 const inquirer = require("inquirer");
+const util = require("util");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-const questions = [];
+/* const writeFileAsync = util.promisify(fs.writeFile); */
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project title?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a brief description of your project.",
+    },
+    {
+      type: "input",
+      name: "install",
+      message: "Provide instructions for installation",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What can your project be used for?",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "License Information",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Who contributed to your project?",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "How to test your project is working?",
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "Where can you be contacted for questions about your project?",
+    },
+  ]);
+}
+/* const questions = [];
 
 function writeToFile(fileName, data) {}
 
 function init() {}
 
-init();
+init(); */
