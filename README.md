aaf5de41c9f86a1a68ae92989380fc4e3870627f
Wesley Slifer <wesleyslifer@ymail.com>
2020-05-10 20:14:24 -0500

    added markdown template literal

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# wes
+## Description 
+asd
+## Table of Contents 
+  * [Installation](##Installation )
+  * [Usage](##Usage)
+  * [License](##License)
+  * [Contributors](##Contributors)
+  * [Test](##Test)
+  * [Contact](##Contact)
+## Installation 
+## Usage 
+## License 
+## Contributors 
+## Test 
+## Contact 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,27 @@
 function generateMarkdown(data) {
   return `
 # ${data.title}
-
+## Description 
+  ${data.description}
+## Table of Contents 
+  * [Installation](##Installation )
+  * [Usage](##Usage)
+  * [License](##License)
+  * [Contributors](##Contributors)
+  * [Test](##Test)
+  * [Contact](##Contact)
+## Installation
+  ${data.install}
+## Usage
+  ${data.usage}
+## License 
+  ${data.license}
+## Contributors 
+  ${data.contributing}
+## Test 
+  ${data.test}
+## Contact 
+  ${data.contact}
 `;
 }
 
